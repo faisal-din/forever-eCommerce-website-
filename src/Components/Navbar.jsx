0d368f9a3d71a5f6f0c456529e95692531f5253e
faisal-din <128973176+faisal-din@users.noreply.github.com>
2024-09-06 16:24:44 +0500

    Update Navbar.jsx

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -49,12 +49,7 @@ const Navbar = () => {
               className="w-5 cursor-pointer"
             />
           </Link>
-          <div className="dropdown-menu hidden group-hover:block absolute ring-0 pt-4 right-2">
-            <div className="flex flex-col gap-2 w-36 py-3 px-5 bg-slate-100 text-gray-500  ">
-              <p className="cursor-pointer hover:text-black">My Profile</p>
-              <p className="cursor-pointer hover:text-black">Orders</p>
-              <p className="cursor-pointer hover:text-black">Logout</p>
-            </div>
+          
           </div>
         </div>
 
